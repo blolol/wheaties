dd075b92a36ad7eaeeea87a11f2785930fde83b2
Ross Paffett <ross@rosspaffett.com>
2010-05-30 02:15:01 -0400

    Add comprehensive handling of plain text commands

--- lib/grunt/evaluator.rb
+++ lib/grunt/evaluator.rb
@@ -13,11 +13,14 @@ module Grunt
       command = Models::Command.first(:name => /^#{name}$/i)
       raise NoCommandError, name unless command
       
-      eval(command.body)
+      eval_method = "eval_#{command.type}"
+      respond_to?(eval_method) ? send(eval_method, command) : nil
     end
     
     def method_missing(method_name, *args)
-      if locals.key?(method_name)
+      if method_name == :locals
+        locals
+      elsif locals.key?(method_name)
         locals[method_name]
       else
         Evaluator.new(method_name.to_s, locals).eval!
@@ -27,6 +30,20 @@ module Grunt
     protected
       def log(level, *args)
         Wheaties::Connection.instance.log(level, *args)
+      end
+      
+      def eval_ruby(command)
+        eval(command.body)
+      end
+      
+      # Remove comments (except for escaped comments), and add a dummy space
+      # to completely blank lines in order to preserve them.
+      def eval_text(command)
+        command.body.gsub(/^\s*(\\\s*)?(#.*)$/) do |match|
+          $1.nil? ? "\0" : match[/^\\\s*(.*)$/, 1]
+        end.gsub(/(\n)?\000\n/) do |match|
+          $1.nil? ? "" : "\n"
+        end.gsub("\n\n", "\n \n")
       end
   end
 end
