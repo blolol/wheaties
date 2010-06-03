938768288ffcaa9cdddb829551d9989bfe9027bf
Ross Paffett <ross@rosspaffett.com>
2010-06-03 03:04:14 -0400

    Detect dynamic command assignments

--- lib/grunt/responses/messages.rb
+++ lib/grunt/responses/messages.rb
@@ -5,8 +5,17 @@ module Grunt
         if (command = is_command?(response.text)) ||
            (response.pm? && command = is_pm_command?(response.text))
           handle_command(command)
+        elsif command = is_assignment?(response.text)
+          log(:debug, %{Dynamically assign text to "#{command[:name]}"})
         end
       end
+      
+      protected
+        def is_assignment?(message)
+          if message =~ /^\s*#{Wheaties::Connection.nick}\s*:\s*([a-zA-Z0-9_]+)\s+is\s+(.*)$/i
+            { :name => $~[1], :text => $~[2] }
+          end
+        end
     end
   end
 end
