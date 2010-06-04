2535012303d284ab875eae9b75b86b8eebd83673
Ross Paffett <ross@rosspaffett.com>
2010-06-04 14:16:08 -0400

    Fill out Models::Command with proper keys and other data

--- lib/grunt/models/command.rb
+++ lib/grunt/models/command.rb
@@ -5,11 +5,22 @@ module Grunt
       
       set_collection_name "commands"
       
-      key :name, String
-      key :body, String
-      key :type, String
+      key :name, String, :required => true
+      key :body, String, :required => true
+      key :desc, String
       key :usage, String
       key :help, Array
+      key :type, String, :required => true
+      key :events, Array
+      key :created_by, String
+      key :updated_by, String
+      timestamps!
+
+      ensure_index :name, :unique => true
+      
+      validates_format_of :name, :with => /^[a-zA-Z0-9_]+$/,
+                          :message => "may contain only alphanumeric characters and underscores"
+      validates_uniqueness_of :name
     end
   end
 end
