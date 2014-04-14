021ac722c366f05ebea8d7bc403292d3dad70fd0
Ross Paffett <ross@rosspaffett.com>
2014-04-14 01:42:46 -0400

    Add support for setting MongoDB port

--- lib/grunt/boot.rb
+++ lib/grunt/boot.rb
@@ -2,50 +2,51 @@ module Grunt
   class << self
     attr_accessor :config
     attr_reader :history
-    
+
     def start
       load_defaults
       load_parser
       load_database
       load_history
     end
-    
+
     def load_defaults
       grunt_config_path = Wheaties.root.join("config/grunt.yml")
       Grunt.config = YAML.load_file(grunt_config_path) || {}
     end
-    
+
     def load_parser
       Treetop.load(File.join(File.dirname(__FILE__), "arguments"))
     end
-    
+
     def load_database
       host = Grunt.config["database"]["host"]
+      port = Grunt.config["database"]["port"] || 27017
       database = Grunt.config["database"]["database"]
-      
-      MongoMapper.connection = Mongo::Connection.new(host)
+
+      MongoMapper.connection = Mongo::Connection.new(host, port)
       MongoMapper.database = database
-      
+
       if Grunt.config["database"]["username"] && Grunt.config["database"]["password"]
         MongoMapper.database.authenticate(Grunt.config["database"]["username"], Grunt.config["database"]["password"])
       end
     end
-    
+
     def load_history
       @history = {}
       history.instance_eval do
         def max_size=(max_size)
           @max_size = max_size
         end
-        
+
         def max_size
           @max_size
         end
-        
+
         def <<(response)
           return unless response.kind_of?(Wheaties::ResponseTypes::OnPrivmsg)
           return if response.pm?
-          
+
           history = (self[response.channel] ||= [])
           history.pop if history.size >= max_size
           history.unshift(response.dup)
@@ -54,6 +55,6 @@ module Grunt
       history.max_size = (Grunt.config["history"] || 50).to_i
     end
   end
-  
+
   self.start
 end
