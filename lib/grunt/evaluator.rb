3dc2807a9b1a3ca642fb1ecdbff198acbcd0ee45
Ross Paffett <ross@rosspaffett.com>
2014-04-14 22:18:36 -0400

    Fix command evaluation in Ruby 2.x
    Object#respond_to? will only look for protected and private methods if
    it is passed `true` as its second argument.

--- lib/grunt/evaluator.rb
+++ lib/grunt/evaluator.rb
@@ -4,26 +4,26 @@ module Grunt
     include Wheaties::Concerns::Formatting
     include Wheaties::Concerns::Logging
     include Wheaties::Concerns::Messaging
-    
+
     attr_reader :name, :locals
-    
+
     EXPOSED_METHODS = [ :desc, :help, :usage, :get, :set, :increment, :inc,
       :send, :pm?, :command?, :event?, :decrement, :dec, :bold, :b, :italic,
       :i, :plain, :pl, :color, :co, :uncolor, :uc, *Wheaties::Concerns::Formatting::COLORS.keys ]
-    
+
     def initialize(name, args = nil, locals = {})
       @name = name
       @args = args.dup unless args.nil?
       @locals = locals.dup
     end
-    
+
     def eval!
       if locals[:level].nil?
         locals[:level] = 0
       else
         locals[:level] += 1
       end
-      
+
       locals[:args] = if @args.is_a?(String) && !@args.empty?
                         parser = ArgumentsParser.new.parse(@args)
                         raise ArgumentParseError, name unless parser
@@ -34,20 +34,20 @@ module Grunt
                         []
                       end
       locals[:args].compact!
-      
+
       if @args.is_a?(String)
         locals[:args].instance_eval do
           def args_string=(args_string)
             @args_string = args_string
           end
-        
+
           def to_s
             @args_string
           end
         end
         locals[:args].args_string = @args
       end
-      
+
       if EXPOSED_METHODS.include?(name.to_sym) && respond_to?(name)
         send(name, *locals[:args])
       elsif command = Command.first(:name => /^#{Regexp.escape(name)}$/i)
@@ -59,7 +59,7 @@ module Grunt
         raise NoCommandError, name
       end
     end
-    
+
     def method_missing(method_name, *args)
       if method_name == :locals
         locals
@@ -70,30 +70,33 @@ module Grunt
         Evaluator.new(method_name.to_s, args, new_locals).eval!
       end
     end
-    
+
     protected
       def eval_command(command)
         command.used!(sender.nick) if primary? && !event?
         eval_method = "eval_#{command.class.name.underscore}"
-        respond_to?(eval_method) ? send(eval_method, command) : nil
+
+        if respond_to?(eval_method, true)
+          send eval_method, command
+        end
       end
-      
+
       def eval_plain_text_command(command)
         result = ERB.new(command.body, nil, "%").result(binding)
         result.gsub(/[\r\n]{2}/, "\n \n") # Preserve blank lines with dummy spaces
       end
-      
+
       def eval_random_line_command(command)
         result = ERB.new(command.body, nil, "%").result(binding)
         result.split(/[\r\n]{2}/).map do |lines|
           lines.split(/[\r\n]/).random
         end.join("\n")
       end
-      
+
       def eval_ruby_command(command)
         eval(command.body)
       end
-      
+
       def eval_yaml_command(command)
         result = ERB.new(command.body, nil, "%").result(binding)
         YAML.load(result)
