a91ea3abeb3ef247d4a6fdd86540595a29288aed
Ross Paffett <ross@rosspaffett.com>
2015-07-01 22:31:09 -0700

    Append message to history after handling command

--- lib/grunt/responses/messages.rb
+++ lib/grunt/responses/messages.rb
@@ -2,15 +2,15 @@ module Grunt
   module Responses
     module Messages
       include Concerns::Matching
-      
+
       def on_privmsg
-        Grunt.history << response
-        
         if response.text =~ command_regex
           handle_command $~[1], $~[2]
         elsif response.text =~ assignment_regex
           handle_assignment $~[1], $~[2]
         end
+
+        Grunt.history << response
       end
     end
   end
