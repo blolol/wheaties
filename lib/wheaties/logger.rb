1babe31afe82e30f49cca45bebc82bd25198ac6c
Ross Paffett <ross@rosspaffett.com>
2018-06-24 20:53:31 -0700

    Log MongoDB activity in development
    * Rename Wheaties::BugsnagLogger to Wheaties::Logger, and make it
      quack more like Ruby's standard library Logger.

---
+++ lib/wheaties/logger.rb
@@ -0,0 +1,57 @@
+module Wheaties
+  class Logger
+    def initialize(bot)
+      @logger = bot.loggers
+    end
+
+    def debug(name, &block)
+      log(:debug, name, &block)
+    end
+
+    def debug?
+      true
+    end
+
+    def error(name, &block)
+      log(:error, name, &block)
+    end
+
+    def error?
+      true
+    end
+
+    def fatal(name, &block)
+      log(:fatal, name, &block)
+    end
+
+    def fatal?
+      true
+    end
+
+    def info(name, &block)
+      log(:info, name, &block)
+    end
+
+    def info?
+      true
+    end
+
+    def log(level, name, &block)
+      messages = if block_given?
+        Array(block.call).map { |message| "#{name} #{message}" }
+      else
+        Array(name)
+      end
+
+      @logger.log(messages, level.to_sym)
+    end
+
+    def warn(name, &block)
+      log(:warn, name, &block)
+    end
+
+    def warn?
+      true
+    end
+  end
+end
