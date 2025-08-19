56efa9bb48c590cb1a51c0fb9eed00935b141dae
Ross Paffett <ross@rosspaffett.com>
2025-08-18 17:41:51 -0700

    Pretty-format Hash argument to debug helper

--- lib/wheaties/helpers/logging_helpers.rb
+++ lib/wheaties/helpers/logging_helpers.rb
@@ -8,10 +8,18 @@ module Wheaties
 
     def debug(*args)
       prefix = "[#{stack.last.name}##{stack.last.id}:#{caller_line_number}]"
-      debug_message = "#{prefix} #{args.map(&:to_s).join(' ')}"
+      debug_message = "#{prefix} #{formatted_args(args)}"
 
       message.user.send(debug_message)
       logger.debug(debug_message)
+    end
+
+    def formatted_args(args)
+      if args.size == 1 && args.first.is_a?(Hash)
+        args.first.map { |key, value| "#{key}=#{value.inspect}" }.join(' ')
+      else
+        args.map(&:to_s).join(' ')
+      end
     end
 
     def logger
