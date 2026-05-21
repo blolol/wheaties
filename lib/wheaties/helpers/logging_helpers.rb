0235fb00750105b313009c18bd529a25a05313e6
Ross Paffett <ross@rosspaffett.com>
2026-05-21 16:07:02 -0700

    Send debug messages to channel instead of PM

--- lib/wheaties/helpers/logging_helpers.rb
+++ lib/wheaties/helpers/logging_helpers.rb
@@ -12,7 +12,7 @@ module Wheaties
       prefix = "[#{stack.last.name}##{stack.last.id}:#{caller_line_number}]"
       debug_message = "#{prefix} #{formatted_args(args)}"
 
-      message.user.send(debug_message)
+      message.reply(debug_message)
       logger.debug(debug_message)
     end
 
